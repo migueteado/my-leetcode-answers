67453518444d9db169435d942b9001a8e1965c70
Miguel Villalobos <27830744+migueteado@users.noreply.github.com>
2026-03-02 17:49:06 -0500

    docs: :memo: Practice 2026-03-02

---
+++ src/00015_3Sum.ts
@@ -0,0 +1,56 @@
+function threeSum(nums: number[]): number[][] {
+  // Sort the array in advance, this reduces complexity
+  // by ordering numbers from lowest to highest
+  nums = nums.sort((a, b) => a - b);
+
+  const triplets: number[][] = [];
+
+  // We will do a for loop where num[i] will be our static number
+  // this facilitates the comparison, with this approach the code inside
+  // the loop is just a twoSum.
+  // We will use a two opposite pointer approach to reduce the complexity
+  for (let i = 0; i < nums.length; i++) {
+    // If we are already at 0 it is impossible to find numbers that
+    // added together result in 0
+    if (nums[i] > 0) {
+      break;
+    }
+
+    // If the previous number is equal to the current number, skip
+    // this is to avoid duplicates
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    // Left pointer, always has to be after the static number
+    let left = i + 1;
+    // Right pointer
+    let right = nums.length - 1;
+    // While ponters have not met
+    while (left < right) {
+      // If the static numbers + pointers result in 0
+      if (nums[i] + nums[left] + nums[right] === 0) {
+        // Push the match
+        triplets.push([nums[i], nums[left], nums[right]]);
+        // We found a match, move both pointers to avoid infinite loop
+        left++;
+        right--;
+        // Skip duplicates, move left and right pointers until we find different numbers
+        while (left < right && nums[left] === nums[left - 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right + 1]) {
+          right--;
+        }
+        // If sum is too high we have to reduce the number, move right pointer
+      } else if (nums[i] + nums[left] + nums[right] > 0) {
+        right--;
+        // Sum is too low we have to increase the number, move left pointer
+      } else {
+        left++;
+      }
+    }
+  }
+
+  return triplets;
+}
---
+++ src/00042_trappingRainWater.ts
@@ -0,0 +1,40 @@
+function trap(height: number[]): number {
+  // Keep pointer for left side
+  let left = 0;
+  // Keep pointer for right side
+  let right = height.length - 1;
+  // Keep track of higher height to the left pointer
+  let leftMax = 0;
+  // Keep track of higher height to the right pointer
+  let rightMax = 0;
+  // Accumulator for the result
+  let result = 0;
+
+  // While our pointers haven't crossed
+  while (left <= right) {
+    // Left height is greater than Right height
+    if (height[left] <= height[right]) {
+      if (height[left] >= leftMax) {
+        // Current is higher, update leftMax
+        leftMax = height[left];
+      } else {
+        // Current is lower, there is space to trap water
+        result += leftMax - height[left];
+      }
+      // Move to the right
+      left++;
+    } else {
+      if (height[right] >= rightMax) {
+        // Current is higher, update rightMax
+        rightMax = height[right];
+      } else {
+        // Current is lower, there is space to trap water
+        result += rightMax - height[right];
+      }
+      // Move to the left
+      right--;
+    }
+  }
+
+  return result;
+}
