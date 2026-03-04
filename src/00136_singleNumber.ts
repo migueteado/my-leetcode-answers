5faf41651ba245e43f2c326c7d34a9ddbf133a57
Miguel Villalobos <27830744+migueteado@users.noreply.github.com>
2026-03-04 10:48:07 -0500

    💡 docs: add comments to singleNumber explaining XOR approach

--- src/00136_singleNumber.ts
+++ src/00136_singleNumber.ts
@@ -1,3 +1,7 @@
+// This is a bit manipulation problem
+// The XOR operation has the property that a ^ a = 0 and a ^ 0 = a
+// So when we XOR all numbers together,
+// the pairs will cancel out and only the single number will remain
 function singleNumber(nums: number[]): number {
   return nums.reduce((acc, num) => acc ^ num, 0);
 }
